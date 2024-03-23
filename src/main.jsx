e8f5b522220451acbf45b67ae380ce32d0b4d729
umerakmal <umerakmal721@gmail.com>
2024-03-23 23:14:20 +0500

    deploy5

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
-import './assets/js/main.js'
+// import './assets/js/main.js'
 import './assets/css/style.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
